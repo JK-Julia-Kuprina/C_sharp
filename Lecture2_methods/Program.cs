5d4697950b751303a93a0d98080a0481bf6bbc83
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-23 00:43:33 +0300

    методы

---
+++ Lecture2_methods/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 1, 12, 31, 41, 5, 16, 41, 7, 18}; 
+
+int n = array.Length;
+int find = 41;
+int index = 0;
+
+while(index<n) {
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
