372eb16629ac4cebb6f373279cf33bb26dd6236a
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-23 16:49:41 +0300

    две задачи

---
+++ Seminar2_task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// ● 14 -> нет
+// ● 46 -> нет
+// ● 161 -> да
+// Напишите программу, которая принимает на вход два
+// числа и проверяет, является ли одно число квадратом
+// другого.
+// ● 5, 25 -> да
+// ● -4, 16 -> да
+// ● 25, 5 -> да
+// ● 8,9 -> нет
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 % 7 == 0 && num1 % 23 == 0){
+//     System.Console.WriteLine("yes");
+// }
+// else{
+//     System.Console.WriteLine("no");
+// }
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 * num1 == num2 || num2 * num2 == num1){
+    System.Console.WriteLine("yes");
+}
+else{
+    System.Console.WriteLine("no");
+}
+
