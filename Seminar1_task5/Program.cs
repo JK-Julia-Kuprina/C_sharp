3c6cb7553b14adb0b60d120aff3b861c47052300
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-25 13:38:54 +0300

    task5

--- Seminar1_task5/Program.cs
+++ Seminar1_task5/Program.cs
@@ -2,3 +2,18 @@
 //  и на выходе показывает третью цифру слева этого числа или говорит, что такой цифры нет. 
 //  Через строку решать нельзя.
 
+System.Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (num > 1000)
+    {
+        num /=  10;
+    }
+    System.Console.WriteLine(num % 10);
+}
