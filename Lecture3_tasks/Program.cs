04d8838610197d3467c10095f7a288795ccef178
Julia Kuprina <131903409+JK-Julia-Kuprina@users.noreply.github.com>
2023-05-28 15:17:51 +0300

    работа с текстом исортировка массива

---
+++ Lecture3_tasks/Program.cs
@@ -0,0 +1,66 @@
+﻿// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к", заменить большими "К",
+// а большие "С", маленькими "с".
+
+// string text = "-Я думаю, - сказал князь улыбаясь, - что, "
+// + "ежели бы вас послали вместо нашего милого Винценгероде,"
+// + "вы бы взяли приступом согласие прусского короля. "
+// + "Вы так красноречивы. Вы дадите мне чаю?";
+// // string s = "qwerty"
+// //             012
+// // s[3]  // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text [i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// System.Console.WriteLine(newText);
+// System.Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// System.Console.WriteLine(newText);
+
+
+// Cортировка массива, расстановка от min к max
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
